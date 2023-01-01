c902b2b3ee547c1aefff63258eca848da1f1a013
Luciano Bitencourt <lucbite@gmail.com>
2023-01-01 10:40:50 -0300

    update
    update

--- js/oo-abstracao.js
+++ js/oo-abstracao.js
@@ -1,46 +1,54 @@
-// Abstração em JS-OO
+// Abstração de Objeto Real em JS-OO
+/**
+ * agencia
+ * numeroConta
+ * numeroAgencia
+ * saldo
+ * limite
+ */
 
-class contaBancaria {
+// Criar  o objeto usando camelCase declarando as prmeiras letras maiusculas
+class ContaBancaria {
 
-    // agencia
-    // numeroConta
-    // saldo
-    // limite
+    /*
+     - Ao se criar os atributos por boas pratica "se declara a segunda palavra com a letra Maiuscula.
+     - Para chamar o atributo se declara o metodo constructor (){} e dentro dele se se declara o operador "this.nomea-do-atributo para chamar  os atributos do objeto". 
 
+    */
     constructor() {
-        this.agencia = 4569;
-        this.numeroConta = 16403;
-        this.saldo = 60;
-        this.limite = 250;
-    }
+            this.agencia = 1075
+            this.numeroConta = 8351125
+            this.saldo = 50
+            this.limite = 450
+        }
+        // fim constructor
+
+    // declara as  ações que  o objeto tem
 
     depositar(valorDepositado) {
         this.saldo += valorDepositado;
     }
 
-    sacar(valorSaque) {
-        this.saldo -= valorSaque;
+    sacar(valorSacado) {
+        this.saldo -= valorSacado;
+        
     }
 
-    consultarSaldo() {
+    
+    verSaldo() {
         return this.saldo
     }
 }
-
-let cc = new contaBancaria()
-let cp = new contaBancaria ()
-
-console.log(cp.consultarSaldo());
-
-cp.depositar(240)
-
-console.log(cp.consultarSaldo());
+// fim objeto 
 
 
-console.log(cc.consultarSaldo());
-cc.depositar(440)
+// Criando  instanciamento do Obejto ContaBancaria
+let cc = new ContaBancaria()
 
-cp.depositar(1500)
 
-console.log(`Saldo C/C ${cc.consultarSaldo().toFixed(2)}`);
-console.log(`Saldo C/P ${cp.consultarSaldo()}`);
+
+
+console.log("Numero da Agencia : " + cc.agencia);
+console.log("Numero da ContaCorrente : " + cc.numeroConta);
+console.log("Saldo da Conta : " + cc.verSaldo().toFixed(2));
+console.log("Saldo do Limite : " + cc.limite.toFixed(2));
