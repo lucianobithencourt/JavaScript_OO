cac028ca10f0724d21a6875541e983e84ef24e4b
Luciano Bitencourt <lucbite@gmail.com>
2023-01-02 06:25:27 -0300

    update
    update

--- js/oo-abstracao.js
+++ js/oo-abstracao.js
@@ -8,18 +8,18 @@
  */
 
 // Criar  o objeto usando camelCase declarando as prmeiras letras maiusculas
-class ContaBancaria {
+/*class ContaBancaria {
 
     /*
      - Ao se criar os atributos por boas pratica "se declara a segunda palavra com a letra Maiuscula.
      - Para chamar o atributo se declara o metodo constructor (){} e dentro dele se se declara o operador "this.nomea-do-atributo para chamar  os atributos do objeto". 
 
-    */
+    /**  
     constructor() {
             this.agencia = 1075
             this.numeroConta = 8351125
             this.saldo = 50
-            this.limite = 450
+            this.limite = 45
         }
         // fim constructor
 
@@ -34,7 +34,7 @@ class ContaBancaria {
         
     }
 
-    
+      
     verSaldo() {
         return this.saldo
     }
@@ -45,6 +45,8 @@ class ContaBancaria {
 // Criando  instanciamento do Obejto ContaBancaria
 let cc = new ContaBancaria()
 
+cc.depositar(450)
+
 
 
 
@@ -52,3 +54,38 @@ console.log("Numero da Agencia : " + cc.agencia);
 console.log("Numero da ContaCorrente : " + cc.numeroConta);
 console.log("Saldo da Conta : " + cc.verSaldo().toFixed(2));
 console.log("Saldo do Limite : " + cc.limite.toFixed(2));
+
+document.write(`Numero da Agencia :  ${cc.agencia} <br><br>`);
+document.write(`Numero da ContaCorrente : ${cc.numeroConta}<br><br>`);
+document.write(`Saldo da Conta :  ${cc.verSaldo().toFixed(2)}<br><br>`);
+document.write(`Saldo do Limite : ${cc.limite.toFixed(2)}<br>`);
+*/
+
+// ABSTRAÇÃO OBJETO Pessoa 
+
+class ContaBancaria {
+    constructor(pa_agencia,pa_conta,pa_saldo,pa_limite){
+        this.agencia = pa_agencia;
+        this.conta = pa_conta;
+        this.saldo = pa_saldo;
+        this.limite = pa_limite;
+    }
+
+    // Metodos 
+
+    depositar(valorDeposito){
+        this.saldo += valorDeposito
+    }
+
+    sacar(valorSaque) {
+        this.saldo -= valorSaque
+    }
+
+    consultarSaldo () {
+        return this.saldo
+    }
+}
+
+// Instaciamento 
+let cpoup = new ContaBancaria ()
+
